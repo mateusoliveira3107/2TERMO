b52f91c4c997fd00bf0bf075a8d9801edb4fc075
Mateus Silva <mateus.oliveira310709@gmail.com>
2026-07-28 18:48:03 -0300

    Ajustes arquivo desafio 2 javascript

--- Backend/desafios/desafio2.js
+++ Backend/desafios/desafio2.js
@@ -1,16 +1,21 @@
 const numeros = [12, 5, 8, 21, 14, 3, 10, 7];
-const pares;
+const pares = [];
 
-for(let num of numeros){
-    if (num % 2 === 0){
+for (let num of numeros) {
+    if (num % 2 === 0) {
         pares.push(num);
     }
 }
 
 // Somando os valores do array
-let soma = 0
-for (let num of pares){
+let soma = 0;
+
+for (let num of pares) {
     soma += num;
 }
 
-const num
+// Calculando a média
+const media = soma / pares.length;
+
+console.log("Números pares:", pares);
+console.log("Média dos números pares:", media.toFixed(2));
