bc425e4f1a629087987342cebe03d316df3fb2e0
mateusoliveira3107 <mateus.oliveira310709@gmail.com>
2026-08-14 11:20:47 -0300

    14/08/2026 Exercícios e arrays com JavaScript

--- Backend/Aula_01/app3.js
+++
@@ -1,7 +0,0 @@
-const escola = "SENAI";
-const nota = 9.5;
-const estaAprovado = true;
-
-console.log("Conteúdo: ", escola, " | Tipo:", typeof escola);
-console.log("Conteúdo: ", nota, "   | Tipo:", typeof nota);
-console.log("Conteúdo: ", estaAprovado, "  | Tipo:", typeof estaAprovado);
--- Backend/Aula_02/Exercicios/contador.js
+++
@@ -1,8 +0,0 @@
-let contador = 0;
-
-while (contador <= 100) {
-    console.log(`Contagem: ${contador}`)
-    contador ++;
-    
-}
-console.log("Fim da contagem!");
