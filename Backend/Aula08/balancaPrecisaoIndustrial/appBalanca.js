471e2d0723d1ff799b954b057fae0918866f0060
Mateus Silva <mateus.oliveira310709@gmail.com>
2026-08-22 17:38:16 -0300

    Organização arquivos exercício js

--- Backend/Aula08/balancaPrecisaoIndustrial/appBalanca.js
+++ Backend/Aula08/balancaPrecisaoIndustrial/appBalanca.js
@@ -2,19 +2,16 @@ const entrada = require('readline-sync');
 const funcao = require('./funcoesBalanca');
 
 while (true) {
-    try {
-        const resposta = entrada.question("Digite o peso da peca [g] ou digite 'sair' para fechar o programa: ");
-        
-        if (resposta.toLowerCase() === "sair") {
-            break;
-    }
-    
-    const peso = Number(resposta);
-    
-    const pesoVerificado = funcao.verificarPeso(peso);
+    try {
+        const resposta = entrada.question("Digite o peso da peça [g] ou digite 'sair' para fechar o programa: ");
+
+        if (resposta.toLowerCase() === "sair") {
+            break;
+        }
+    const pesoVerificado = funcao.verificarPeso(resposta);
     console.log(pesoVerificado);
-    
-    } catch (erro) {
-        console.log(`ALERTA: Entrada inválida.\n`);
+
+    } catch (erro) {
+        console.log(`Alerta: ${erro.message}`);
     }
 }
